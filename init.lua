e413822ad5c424f758e726ec33493ef541fea48f
victorisikwei <victordumbiri88@gmail.com>
2025-02-28 02:18:49 +0300

    new life

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 vim.opt.guicursor = "n-v-c:block,i:block"
--- vim.opt.cursorline = true
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 0
 vim.opt.wildmenu = false
 vim.opt.tabstop = 4
 -- vim.opt.scrolloff = 8
@@ -22,13 +21,14 @@ vim.opt.linespace = 10
 vim.opt.smartindent = true
 vim.opt.updatetime = 80
 vim.opt.timeoutlen = 80
-vim.opt.autochdir = true  -- change the current working directory whenever you open a file
+vim.opt.autochdir = true -- change the current working directory whenever you open a file
 vim.opt.list = true
-vim.opt.listchars = { space = "·" }
-vim.opt.virtualedit="all"
+vim.opt.listchars = { tab = ".." }
+vim.opt.virtualedit = "all"
 vim.g.mapleader = " "
 -- vim.opt.clipboard = "unnamedplus"
 vim.opt.fillchars:append({ eob = " " }) -- remove [~] character from nvim
+
 --[--------------------------------]
 -----Custom SatusLine
 -- function MyStatusLine()
@@ -40,17 +40,17 @@ vim.opt.fillchars:append({ eob = " " }) -- remove [~] character from nvim
 
 -------NEOVIDE STUFFS--------
 if vim.g.neovide then
-    vim.o.guifont = "FantasqueSansM Nerd Font:h5.9:b" -- text below applies for VimScript
-    -- vim.o.guifont = "FantasqueSansM Nerd Font:h5.4" -- text below applies for VimScript
-    -- vim.g.neovide_hide_mouse_when_typing = true
-    -- vim.g.neovide_cursor_animation_length = 0.13
-    vim.g.neovide_cursor_trail_size = 0.8
-    -- vim.g.neovide_cursor_animation_length = 0.05
-    -- vim.g.neovide_cursor_vfx_mode = "torpedo"
-    vim.g.neovide_cursor_smooth_blink = true
-    vim.g.neovide_cursor_vfx_mode = "sonicboom"
-    vim.g.neovide_cursor_vfx_mode = "railgun"
-    vim.g.neovide_cursor_vfx_mode = "pixiedust"
+	vim.o.guifont = "FantasqueSansM Nerd Font:h5.9:b" -- text below applies for VimScript
+	-- vim.o.guifont = "FantasqueSansM Nerd Font:h5.4" -- text below applies for VimScript
+	-- vim.g.neovide_hide_mouse_when_typing = true
+	-- vim.g.neovide_cursor_animation_length = 0.13
+	vim.g.neovide_cursor_trail_size = 0.8
+	-- vim.g.neovide_cursor_animation_length = 0.05
+	-- vim.g.neovide_cursor_vfx_mode = "torpedo"
+	vim.g.neovide_cursor_smooth_blink = true
+	vim.g.neovide_cursor_vfx_mode = "sonicboom"
+	vim.g.neovide_cursor_vfx_mode = "railgun"
+	vim.g.neovide_cursor_vfx_mode = "pixiedust"
 end
 ---------KEYMAPS---------
 --- move stuff
@@ -61,7 +61,10 @@ vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set('i', '<BS>', '<NOP>', {noremap = true})
+vim.keymap.set("v", "<", "<gv^")
+vim.keymap.set("v", ">", ">gv^")
+
+vim.keymap.set("i", "<BS>", "<NOP>", { noremap = true })
 vim.keymap.set("n", "j", "jzz")
 vim.keymap.set("n", "k", "kzz")
 vim.keymap.set("n", "<C-q>", "<cmd>%bd|e#<CR>")
@@ -73,13 +76,13 @@ vim.keymap.set("i", "jk", "<Esc>") -- easy escape
 vim.keymap.set("i", "jj", "<Esc>") -- easy escape
 vim.keymap.set("i", "kk", "<Esc>") -- easy escape
 vim.keymap.set("i", "kj", "<Esc>") -- easy escape
-vim.keymap.set("n", "<leader>d", ":bd!<CR>")
+vim.keymap.set("n", "<leader>q", ":bd!<CR>")
 vim.keymap.set("n", "<leader>1", "<cmd>silent only!<CR>") -- NOTE: check <cmd> vs ":"
 -- vim.keymap.set("n", "<C-f>", ":e ~/.config/nvim/init.lua<CR>")
 -- Tabs and switching tabs
-vim.keymap.set("n", "<C-n>", "<cmd>tabnew<CR>", {silent = true})
-vim.keymap.set("n", "<Tab>", "<cmd>tabn<CR>", {silent = true})
-vim.keymap.set("n", "<S-Tab>", "<cmd>tabp<CR>", {silent = true})
+vim.keymap.set("n", "<C-n>", "<cmd>tabnew<CR>", { silent = true })
+vim.keymap.set("n", "<Tab>", "<cmd>tabn<CR>", { silent = true })
+vim.keymap.set("n", "<S-Tab>", "<cmd>tabp<CR>", { silent = true })
 -- Toggle Filemanager
 vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>")
 vim.keymap.set("n", "<leader>4", "$")
@@ -91,79 +94,89 @@ vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("i", "<C-S-V>", "<C-R>+")
 -- Scaling
-vim.keymap.set("n", "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", { silent = true })
-vim.keymap.set("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", { silent = true })
+vim.keymap.set(
+	"n",
+	"<C-=>",
+	":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
+	{ silent = true }
+)
+vim.keymap.set(
+	"n",
+	"<C-->",
+	":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
+	{ silent = true }
+)
 vim.keymap.set("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
 --- Compile
-vim.keymap.set('n', '<leader>t', ':19sv<CR>:enew<CR>:terminal ')
+vim.keymap.set("n", "<leader>t", ":15sv<CR>:enew<CR>:terminal ")
 -- terminal
-vim.keymap.set('n', '<C-t>t', '<cmd>tabnew<CR>')
+vim.keymap.set("n", "<C-t>t", "<cmd>tabnew<CR>")
 -- Resize with arrows
 vim.keymap.set("n", "<C-Up>", ":resize +1<CR>")
 vim.keymap.set("n", "<C-Down>", ":resize -1<CR>")
 vim.keymap.set("n", "<C-Left>", ":vertical resize +1<CR>")
 vim.keymap.set("n", "<C-Right>", ":vertical resize -1<CR>")
 -- Different keys for system registery yank and paste
-vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("n", "<leader>p", "\"+p")
-vim.keymap.set("v", "<leader>p", "\"+p")
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
-vim.keymap.set("v", "<leader>y", "\"+y")
--- vim.keymap.set("n", "<leader>d", "\"_d")-----deleting into the void register N:mode
-vim.keymap.set("v", "<leader>d", "\"_d") --- deleting into the void register V:mode
+vim.keymap.set("x", "<leader>p", '"_dP')
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>p", '"+p')
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>d", '"_d') -----deleting into the void register N:mode
+vim.keymap.set("v", "<leader>d", '"_d') --- deleting into the void register V:mode
 vim.keymap.set("n", "Y", "Vy")
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 ---- Colors and som Vim script stuffs ------
 vim.cmd([[
-" colorscheme iceberg
-highlight VertSplit ctermbg=NONE guibg=NONE
-highlight WinSeparator guibg=NONE" ctermbg=NONE guifg=#55587A
-set laststatus=3
-]])
+	" colorscheme iceberg
+	highlight VertSplit ctermbg=NONE guibg=NONE
+	highlight WinSeparator guibg=NONE" ctermbg=NONE guifg=#55587A
+	set laststatus=3
+	]])
 --{-------------------------------------------------------------
--- Show cursorline only in the active window                    
+-- Show cursorline only in the active window
 vim.api.nvim_create_autocmd("WinEnter", {
-    pattern = "*",
-    callback = function()
-        -- vim.wo.cursorline = true
-    end,
+	pattern = "*",
+	callback = function()
+		-- vim.wo.cursorline = true
+	end,
 })
 -- Turn-off cursorline on window leave
 vim.api.nvim_create_autocmd("WinLeave", {
-    pattern = "*",
-    callback = function()
-        -- vim.wo.cursorline = false
-    end,
+	pattern = "*",
+	callback = function()
+		-- vim.wo.cursorline = false
+	end,
 })
 --{--------------------------------------------------------------|---}
 -- Scratch buffer
 vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function()
-        local bufname = vim.api.nvim_buf_get_name(0)
-        if bufname == "" then
-            vim.bo.buftype = "nofile"  -- Make it a scratch buffer
-            vim.bo.bufhidden = "hide"  -- Hide when abandoned
-            vim.bo.swapfile = false    -- No swap file
-        end
-    end
+	callback = function()
+		local bufname = vim.api.nvim_buf_get_name(0)
+		if bufname == "" then
+			vim.bo.buftype = "nofile" -- Make it a scratch buffer
+			vim.bo.bufhidden = "hide" -- Hide when abandoned
+			vim.bo.swapfile = false -- No swap file
+		end
+	end,
 })
 
 -- Terminal mode
-vim.api.nvim_create_autocmd('TermOpen', {
-    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-    callback = function()
-        vim.opt.number = false
-        vim.opt.relativenumber = false
-    end
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+	end,
 })
 
 -- NOTE:  Deal with this later
