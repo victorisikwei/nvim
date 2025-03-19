27c548ef9df4caba1b2b94154d0707d4e19bc954
victorisikwei <victordumbiri88@gmail.com>
2025-03-19 07:55:58 +0300

    backgroud color

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		-- vim.opt.termguicolors = true
 		vim.cmd.colorscheme("melange")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "#0C0C0C", fg = "#B66969" })
+		vim.api.nvim_set_hl(0, "Normal", { bg = "#1B1B1B", fg = "#B66969" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = "none" })
 		vim.api.nvim_set_hl(0, "CursorLine", { bg = "#110707" })
