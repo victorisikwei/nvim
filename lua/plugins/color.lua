8883c1cd40c87b7f9acf50f8111eebb92591e7db
victorisikwei <victordumbiri88@gmail.com>
2025-03-23 14:00:03 +0300

    mx

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -5,11 +5,12 @@ return {
 		-- vim.opt.termguicolors = true
 		vim.cmd.colorscheme("melange")
 		vim.api.nvim_set_hl(0, "Normal", { bg = "#0E0E0E", fg = "#E2BC9C" })
+		vim.api.nvim_set_hl(0, "Visual", { bg = "#2C2C2C", fg = "#DB7D73" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = "none" })
 		vim.api.nvim_set_hl(0, "CursorLine", { bg = "#140608" })
 		vim.api.nvim_set_hl(0, "Whitespace", { fg = "#2A2A2A" })
-		vim.api.nvim_set_hl(0, "Comment", { fg = "#927E7E" })
+		vim.api.nvim_set_hl(0, "Comment", { fg = "#3B768D" })
 		vim.api.nvim_set_hl(0, "Cursor", { fg = "#E2BC9C" })
 		vim.api.nvim_set_hl(0, "lCursor", { bg = "#E2BC9C" })
 	end,
